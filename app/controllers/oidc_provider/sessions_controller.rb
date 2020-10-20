f04a5810b645538aafebc0781ef2c95ce39320f7
Ashley Hutton <ashley@brandnewbox.com>
2020-10-20 13:50:55 -0500

    Pass params

--- app/controllers/oidc_provider/sessions_controller.rb
+++ app/controllers/oidc_provider/sessions_controller.rb
@@ -5,7 +5,7 @@ module OIDCProvider
     def destroy
       unauthenticate!
       redirect_to "/" unless OIDCProvider.after_sign_out_path
-      redirect_to OIDCProvider.after_sign_out_path.respond_to?(:call) ? OIDCProvider.after_sign_out_path.call : OIDCProvider.after_sign_out_path
+      redirect_to OIDCProvider.after_sign_out_path.respond_to?(:call) ? OIDCProvider.after_sign_out_path.call(params) : OIDCProvider.after_sign_out_path
     end
   end
 end
