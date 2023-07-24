3251a1fc3b5aae7220dabbce7de06455c0e14c99
Guillaume Hain <zedtux@zedroot.org>
2023-07-24 15:25:20 +0200

    Implements basic claims request parameter (Closes #11)

--- db/migrate/20170928211111_create_oidc_provider_authorizations.rb
+++ db/migrate/20170928211111_create_oidc_provider_authorizations.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class CreateOIDCProviderAuthorizations < ActiveRecord::Migration[5.1]
   def change
     create_table :oidc_provider_authorizations do |t|
-      t.references :account, foreign_key: {to_table: OIDCProvider.account_class.tableize}, null: false
+      t.references :account, foreign_key: { to_table: OIDCProvider.account_class.tableize }, null: false
       t.string :client_id, null: false
       t.string :nonce
       t.string :code, null: false
       t.text :scopes, null: false
+      t.text :claims
       t.datetime :expires_at, null: false
 
       t.timestamps
--- lib/oidc_provider/account_to_user_info.rb
+++ lib/oidc_provider/account_to_user_info.rb
@@ -1,12 +1,25 @@
+# frozen_string_literal: true
+
 module OIDCProvider
   class AccountToUserInfo
-    def call(account, scope_names)
-      scopes = scope_names.map { |name| OIDCProvider.supported_scopes.detect { |scope| scope.name == name } }.compact
-      OpenIDConnect::ResponseObject::UserInfo.new(sub: account.send(OIDCProvider.account_identifier)).tap do |user_info|
+    def initialize(scope_names)
+      @scope_names = scope_names
+    end
+
+    def call(account)
+      OpenIDConnect::ResponseObject::UserInfo.new(
+        sub: account.send(OIDCProvider.account_identifier)
+      ).tap do |user_info|
         scopes.each do |scope|
-          UserInfoBuilder.new(user_info, account).run(&scope.work)
+          ResponseObjectBuilder.new(user_info, account).run(&scope.work)
         end
       end
+    end
+
+    private
+
+    def scopes
+      @scope_names.map { |name| OIDCProvider.find_scope(name) }.compact
     end
   end
 end
